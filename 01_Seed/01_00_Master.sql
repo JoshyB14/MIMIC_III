714bc7c911eee03a999cadf75cd2327d19a4e68b
Josh Bryden <54197391+JoshyB14@users.noreply.github.com>
2023-12-14 20:14:53 +1100

    Create 01_00_Master.sql

---
+++ 01_Seed/01_00_Master.sql
@@ -0,0 +1,5 @@
+-- PSQL commands to run all MIMIC-III table seed scripts
+
+\i '/Users/joshbryden/Desktop/github/MIMIC_III/01_Seed/01_01_Admissions.sql'
+\i '/Users/joshbryden/Desktop/github/MIMIC_III/01_Seed/01_02_antibiotics.sql'
+
